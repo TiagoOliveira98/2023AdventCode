135349fb71d7a0d05f730ad5ff9bc561b79e99e7
TiagoOliveira98 <tiago.mmadalenooliveira@gmail.com>
2023-12-14 17:13:00 +0000

    First version of the solution for Day 1

---
+++ Day1/main.py
@@ -0,0 +1,30 @@
+file = open('data.txt', 'r')
+lines = file.readlines()
+
+spellNumbers ={"one":1, "two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9, "1":1, "2":2, "3":3, "4":4, "5":5 ,"6":6, "7":7, "8":8, "9":9}
+all = []
+sum = 0
+
+for line in lines:
+    init = 0
+    last = 0
+    aux = []
+    while last < len(line):
+        #print(init)
+        #print(last)
+        for i in spellNumbers:            
+            if i in line[init:last+1]:
+                #print(i)
+                if i not in ["1","2","3","4","5","6","7","8","9"]:
+                    init = last
+                else:
+                    init = last + 1 
+                aux.append(spellNumbers[i])
+        last += 1
+        #print(aux)
+    sum += aux[0]*10 + aux[-1]
+    all.append(sum)
+    print(sum)
+
+print(len(all))
+print 
