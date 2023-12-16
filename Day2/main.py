deee792dc82b1730cbce378e4659e3fb013eba33
TiagoOliveira98 <tiago.mmadalenooliveira@gmail.com>
2023-12-16 12:17:12 +0000

    Clean main branch

--- Day2/main.py
+++
@@ -1,9 +0,0 @@
-file = open('data.txt', 'r')
-lines = file.readlines()
-
-strings = ['12 red', '13 green', '13 blue']
-
-# out = list([idx+1 if (strings[0] in line or strings[1] in line or strings[2] in line) else 0 for (idx,line) in enumerate(lines)])
-# print(out)
-
-print(list(map(lambda line: line.split(':')[1], lines)))
